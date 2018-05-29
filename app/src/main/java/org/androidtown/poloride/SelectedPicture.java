b28b2323bff10c5efd3ccbfe7a25a08c3335f39f
DESKTOP-NU84P3P\a <rhkrthdjs>
2018-05-29 16:12:13 +0900

    SelectedPicture 액티비티

---
+++ app/src/main/java/org/androidtown/poloride/SelectedPicture.java
@@ -0,0 +1,13 @@
+package org.androidtown.poloride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectedPicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_picture);
+    }
+}
