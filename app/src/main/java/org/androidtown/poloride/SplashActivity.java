1bd48d3ca000b2380065a097109bc7a1e2299ff4
KANGBOEUN <boeunrosie@gmail.com>
2018-06-12 00:27:58 +0900

    2

--- app/src/main/java/org/androidtown/poloride/SplashActivity.java
+++ app/src/main/java/org/androidtown/poloride/SplashActivity.java
@@ -1,13 +1,30 @@
 package org.androidtown.poloride;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private Handler handler;
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash2);
+        init();
+
+        handler.postDelayed(runnable, 3000);
+    }
+    public void init() {
+        handler = new Handler();
     }
 }
